517f5441cc3c0faf62a2ef7a92912f0ee6fee511
gourab8389 <deyg6988@gmail.com>
2024-07-30 12:21:45 +0530

    add collaborator deletemodels and other

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,30 +1,34 @@
 import { liveblocks } from "@/lib/liveblocks";
+import { getUserColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 export async function POST(request: Request) {
-    const clerkUser = await currentUser();
+  const clerkUser = await currentUser();
 
-    if(!clerkUser) redirect('/sign-in')
+  if(!clerkUser) redirect('/sign-in');
 
-        const {id, firstName, lastName, emailAddresses, imageUrl} = clerkUser;
+  const { id, firstName, lastName, emailAddresses, imageUrl } = clerkUser;
+
   // Get the current user from your database
   const user = {
     id,
-    info:{
-        name: `${firstName} ${lastName}`,
-        email:emailAddresses[0].emailAddress,
-        avatar:imageUrl
+    info: {
+      id,
+      name: `${firstName} ${lastName}`,
+      email: emailAddresses[0].emailAddress,
+      avatar: imageUrl,
+      color: getUserColor(id),
     }
   }
+
   // Identify the user and return the result
   const { status, body } = await liveblocks.identifyUser(
     {
-      userId: user.id,
-      groupIds, // Optional
+      userId: user.info.email,
+      groupIds: [],
     },
-    { userInfo: user.metadata },
+    { userInfo: user.info },
   );
 
   return new Response(body, { status });
